f32b4f144cf489530c07f71a17fb4771fc4582fc
Shi <mo@shi.moe>
2022-05-18 06:30:31 +0200

    init

---
+++ data.js
@@ -0,0 +1,88 @@
+export const data = {
+  player: {
+    name: '',
+    health: 100,
+    maxHealth: 100,
+    level: 1,
+    exp: 0,
+    expToNextLevel: 100,
+    copper: 0,
+    silver: 0,
+    gold: 0,
+    toolAxe: "Hands",
+    toolPickaxe: "Hands",
+    toolShovel: "Hands",
+    weapon: "Hand",
+  },
+  explore: [
+    {
+      id: 0,
+      title: 'Drumrain Forest',
+      desc: 'A forest full of trees and bushes.',
+    }, {
+      id: 1,
+      title: 'Tarhill Mountain',
+      desc: 'A mountain.',
+    }, {
+      id: 2,
+      title: 'Ashrust Cave',
+      desc: 'A forest full of trees and bushes.',
+    }, {
+      id: 3,
+      title: 'Farstream River',
+      desc: 'A forest full of trees and bushes.',
+    }, {
+      id: 4,
+      title: 'Deadsoil Desert',
+      desc: 'A forest full of trees and bushes.',
+    },
+  ],
+  items: [
+    {
+      id: 0,
+      type: "resource",
+      name: "Branch",
+      desc: "A branch.",
+      amount: 0,
+      value: 1,
+    }, {
+      id: 1,
+      type: "resource",
+      name: "Wood",
+      desc: "A piece of wood.",
+      amount: 0,
+      value: 1,
+    }, {
+      id: 2,
+      type: "resource",
+      name: "Redwood",
+      desc: "A redwood.",
+      amount: 0,
+      value: 1,
+    }, {
+      id: 3,
+      type: "resource",
+      name: "Ironwood",
+      desc: "An ironwood.",
+      amount: 0,
+      value: 1,
+    }, {
+      id: 4,
+      type: "resource",
+      name: "Flint",
+      desc: "A flint.",
+      amount: 0,
+      value: 1,
+    }, {
+      id: 5,
+      type: "resource",
+      name: "Stone",
+      desc: "A stone.",
+      amount: 0,
+      value: 1,
+    }, {
+      id: 6,
+      type: "resource",
+      
+  ]
+};
